232dd8c1e2468e195409a28778e66deda0f26c09
Nico <nicoalearriola@gmail.com>
2024-08-14 23:45:29 -0300

    agregacion de nueva implementacion para futuro

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -13,30 +13,32 @@ function LoginPage() {
         console.log(data)}) // ejecucion de handleSubmit donde me va a devolver los datos
     return(
         <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
-            <form onSubmit={onSubmit}> 
-                {/* Captura los valores tipeados por el usuario */}
-                
-                <input 
-                    type="email" 
-                    {...register('email', {required: true})}
-                    className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
-                    placeholder='Email'
-                />
-                {errors.email && (<p className="text-red-500"> email is required</p>
-                )} {/* mensaje error de formState */} 
+           <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
+                <form onSubmit={onSubmit}> 
+                        {/* Captura los valores tipeados por el usuario */}
+                        <input 
+                            type="email" 
+                            {...register('email', {required: true})}
+                            className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+                            placeholder='Email'
+                        />
+                        {errors.email && (<p className="text-red-500"> email is required</p>
+                        )} {/* mensaje error de formState */} 
 
-                <input 
-                    type="password" 
-                    {...register('password',{required: true})}
-                    className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
-                    placeholder='Password'
-                />
-                {errors.password && (<p className="text-red-500"> password is required</p>
-                )} {/* mensaje error de formState */} 
+                        <input 
+                            type="password" 
+                            {...register('password',{required: true})}
+                            className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+                            placeholder='Password'
+                        />
+                        {errors.password && (<p className="text-red-500"> password is required</p>
+                        )} {/* mensaje error de formState */} 
 
-                {/* Botón de registro */}
-                <button type="submit"> Register </button>
-            </form>
+                        {/* Botón de registro */}
+                        <button type="submit"> Register </button>
+                        <div>button</div>
+                </form>
+           </div>
         </div>
     )
 }
